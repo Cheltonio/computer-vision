67ff70e4de6bb585a88d5fc646d255b3c81a6532
Chelton Forbes <cj.p.forbes@gmail.com>
2023-08-10 14:22:47 -0400

    first commit

--- sketch.js
+++ sketch.js
@@ -1,40 +1,91 @@
 let myXPos = 100;
 let myYPos = 100;
 
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/kUxfDqAHL/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+function preload() {
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+}
 
 function setup() {
-   createCanvas(500, 500);
-   noStroke();
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
 
-
-   rectMode(CENTER);
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
+        
+    noStroke();
+    rectMode(CENTER);
 }
 
 
 function draw() {
-   background(0);
+    background(0);
+
+    image(flippedVideo, 0, 0);
+
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+
+    fill(255, 0, 0);
+    rect(myXPos, myYPos, 50, 50);
 
 
-   fill(255, 0, 0);
-   rect(myXPos, myYPos, 50, 50);
+    if (label=="Left") {
+        myXPos -= 3;
+    }
 
 
-   if (keyIsDown(LEFT_ARROW)) {
-       myXPos -= 3;
-   }
+    if (label=="Right") {
+        myXPos += 3;
+    }
 
 
-   if (keyIsDown(RIGHT_ARROW)) {
-       myXPos += 3;
-   }
+    if (label=="Up") {
+        myYPos -= 3;
+    }
 
 
-   if (keyIsDown(UP_ARROW)) {
-       myYPos -= 3;
-   }
-
-
-   if (keyIsDown(DOWN_ARROW)) {
-       myYPos += 3;
-   }
+    if (label=="Down") {
+        myYPos += 3;
+    }
 }
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+
+  }
+
+  // When we get a result
+  function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+      console.error(error);
+      return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
+    // Classifiy again!
+    classifyVideo();
+  }
